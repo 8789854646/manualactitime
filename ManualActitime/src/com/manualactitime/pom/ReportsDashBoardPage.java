fcdc322cd8aac62e0d560f1fd870f22b5cad7f23
kushw <kushw@VIVEK>
2022-05-08 19:09:14 +0530

    vivek kr 1st

---
+++ ManualActitime/src/com/manualactitime/pom/ReportsDashBoardPage.java
@@ -0,0 +1,5 @@
+package com.manualactitime.pom;
+
+public class ReportsDashBoardPage {
+
+}
---
+++ ManualActitime/src/com/manualactitime/testscript/CustomerModule.java
@@ -0,0 +1,43 @@
+package com.manualactitime.testscript;
+
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import com.manualactitime.generic.BaseClass;
+import com.manualactitime.generic.FileLib;
+import com.manualactitime.pom.EnterTimeTrackPage;
+import com.manualactitime.pom.TaskListPage;
+@Listeners(com.manualactitime.generic.ListenersImplementation.class)
+public class CustomerModule extends BaseClass {
+	@Test
+	public void testCreateCustomer() throws InterruptedException, EncryptedDocumentException, IOException
+	{
+		Reporter.log("CreateCustomer",true);
+		FileLib f=new FileLib();
+		String custName = f.getExcelData("CreateCustomer", 1, 2);
+		String custDesc = f.getExcelData("CreateCustomer", 1, 3);
+		EnterTimeTrackPage e=new EnterTimeTrackPage(driver);
+		e.clickTaskTab();
+		TaskListPage t=new TaskListPage(driver);
+		t.getAddNewBtn().click();
+		t.getNewCustOption().click();
+		t.getEnterCustNameTbx().sendKeys(custName);
+		t.getEnterCustDescTbx().sendKeys(custDesc);
+		t.getSelectCustDD().click();
+		t.getOurCompany().click();
+		t.getCreateCustBtn().click();
+	    WebDriverWait wait=new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.textToBePresentInElement(t.getActualCustCreated(), custName));
+		String actualCustText = t.getActualCustCreated().getText();
+		Assert.assertEquals(actualCustText, custName);
+		Reporter.log("Customer successfully created ="+custName,true);
+		f.setExcelData("CreateCustomer", 1, 4, "pass");
+	}
+}
